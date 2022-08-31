ad80e48775d353bc6cd24877964856f0f57893ad
Jay Harris <jay.harris@outlook.co.nz>
2022-08-31 13:40:54 +1200

    Setup

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.ts',
+  plugins: [resolve(), typescript()],
+  output: [
+    { format: 'cjs', file: pkg.main, exports: 'auto' },
+    { format: 'esm', file: pkg.module }
+  ]
+};
